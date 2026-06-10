49dcb6765c3edc69a704df7b7a1f0c343f7b0240
Prime <your@email.com>
2026-06-10 14:41:53 +0530

    Colour file added

---
+++ app/src/main/java/com/muwan/muwanchat/Colors.kt
@@ -0,0 +1,10 @@
+package com.muwan.muwanchat
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBg = Color(0xFF1a1a2e)
+val DarkHeader = Color(0xFF16213e)
+val DarkAccent = Color(0xFFff6b35)
+val DarkInputBg = Color(0xFF0f3460)
+val DarkBubbleSent = Color(0xFFff6b35)
+val DarkBubbleReceived = Color(0xFF16213e)
